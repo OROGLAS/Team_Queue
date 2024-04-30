d8f59dd7aa4f9c4a4de9c16ad19d894f42d394b1
OROGLAS <146229323+OROGLAS@users.noreply.github.com>
2024-04-30 22:20:20 +0300

    Update Artem.cpp

--- Artem.cpp
+++ Artem.cpp
@@ -1,1 +1,23 @@
-Кидай все сюда потом разберемся
+#include <iostream>
+#include <queue>
+
+void showQueueInfo(std::queue<int>& q) {
+    if (q.empty()) {
+        std::cout << "Очередь пуста." << std::endl;// Проверка на пуст
+        return;
+    }
+
+    // Вывод количества элементов в очереди
+    std::cout << "Количество элементов в очереди: " << q.size() << std::endl;
+
+    // Вывод первого элемента очереди (головы)
+    std::cout << "Первый элемент очереди (голова): " << q.front() << std::endl;
+
+    // Вывод последнего элемента очереди (хвоста)
+    std::queue<int> tempQueue = q;
+    int lastElement;
+    while (!tempQueue.empty()) {
+        lastElement = tempQueue.front();
+        tempQueue.pop();
+    }
+    std::cout << "Последний элемент очереди (хвост): " << lastElement << std::endl;
